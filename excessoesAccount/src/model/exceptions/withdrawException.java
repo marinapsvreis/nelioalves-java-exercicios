4e08018409da60f706e841c0197dd7418aa8ee58
Marina Portugal <marinapsvreis@gmail.com>
2022-04-13 22:47:27 -0300

    ultimo exercicio excessoes

---
+++ excessoesAccount/src/model/exceptions/withdrawException.java
@@ -0,0 +1,11 @@
+package model.exceptions;
+
+public class withdrawException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public withdrawException(String msg) {
+		super(msg);
+	}
+
+}
