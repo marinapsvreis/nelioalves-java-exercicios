7effea3bf9ba4c0fddb2a52e8b22749e694330d5
Marina Portugal <marinapsvreis@gmail.com>
2022-04-20 18:04:56 -0300

    comparable e gerenerics

---
+++ EmprestimoCalculo/src/services/BrazilianInterestService.java
@@ -0,0 +1,15 @@
+package services;
+
+public class BrazilianInterestService implements InterestService {
+	private double interestRate;
+
+	public BrazilianInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+}
